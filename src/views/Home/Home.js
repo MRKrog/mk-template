fab6e2fb7c3f708fb8e77c1e26e1541387a31f3a
Michael Krog <michaelryankrog@gmail.com>
2019-12-03 13:55:42 -0600

    created routes for home and about

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux";
-import * as actions from "../../redux/actions";
+import React from 'react';
+// import { connect } from "react-redux";
+// import * as actions from "../../redux/actions";
 
 const Home = () => {
   return (
